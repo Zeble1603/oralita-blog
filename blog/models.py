c2e9e87841e52985578f960cb629bbaadcd3a756
Zeblé <66326476+Zeble1603@users.noreply.github.com>
2021-11-18 23:00:26 +0100

    first part modelisation

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Tag(models.Model):
+    nombre = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Post(models.Model):
+    titulo = models.CharField(max_length=200)
+    texto = models.TextField()
+    tags = models.ManyToManyField(Tag,null=True)
+
+
+
