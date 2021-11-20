56416d0abf0b746d7b4e7583eb575716928aa875
Zeblé <66326476+Zeble1603@users.noreply.github.com>
2021-11-20 12:58:29 +0100

    first base for modelisation

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -12,7 +13,23 @@ class Tag(models.Model):
 class Post(models.Model):
     titulo = models.CharField(max_length=200)
     texto = models.TextField()
+    resumen = models.CharField(max_length=200)
+    fecha_publicacion = models.DateTimeField(blank=True,null=True)
     tags = models.ManyToManyField(Tag,null=True)
+
+    class Meta:
+        ordering = ['fecha_publicacion']
+
+    def __str__(self):
+        return self.titulo
+
+    def publicar(self):
+        self.fecha_publicacion = timezone.now()
+        self.save()
+
+    
+
+
 
 
 
