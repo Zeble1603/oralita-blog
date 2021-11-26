a43c6d5a72e7f6a45caec7cc927b2cf287574da1
Zeblé <66326476+Zeble1603@users.noreply.github.com>
2021-11-26 15:52:52 +0100

    tags as tick boxes

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
-from . import models
 from django.contrib.auth.models import Group
+
+
+from . import models
+
 # Register your models here.
 
 admin.site.site_header = 'Administración del Blog de Oralita'
 
-admin.site.register(models.Articulo)
+admin.site.register(models.Articulo,models.MyModelAdmin)
 admin.site.register(models.Tag)
 admin.site.unregister(Group)
--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,19 @@
+from django.contrib import admin
 from django.db import models
+from django.db.models import ManyToManyField
+from django.forms import CheckboxSelectMultiple
 from django.utils import timezone
 
 from ckeditor.fields import RichTextField
 from ckeditor_uploader.fields import RichTextUploadingField
 # Create your models here.
+
+
+class MyModelAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        ManyToManyField: {'widget': CheckboxSelectMultiple},
+    }
+
 
 class Tag(models.Model):
     nombre = models.CharField(max_length=200)
@@ -16,7 +26,7 @@ class Articulo(models.Model):
     titulo = models.CharField(max_length=200)
     photo = models.ImageField(blank=True,null=True,upload_to="images")
     texto = RichTextUploadingField(blank=True, null=True)
-    resumen = models.CharField(max_length=200)
+    resumen = models.TextField(max_length=500)
     fecha = models.DateField(blank=True,null=True)
     tags = models.ManyToManyField(Tag,blank=True)
     publicado = models.BooleanField(default=False)
