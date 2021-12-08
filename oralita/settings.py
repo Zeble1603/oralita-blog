b0b551156b17b197b406267ac524d926d24f2d7c
Zeblé <66326476+Zeble1603@users.noreply.github.com>
2021-12-08 19:08:48 +0100

    Update settings.py

--- oralita/settings.py
+++ oralita/settings.py
@@ -184,6 +184,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
