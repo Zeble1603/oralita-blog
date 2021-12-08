2d0c7fccd9d2865f8dff9f993b346259fa2571b1
Zeblé <66326476+Zeble1603@users.noreply.github.com>
2021-12-08 18:40:35 +0100

    whitenoise

--- oralita/settings.py
+++ oralita/settings.py
@@ -129,6 +129,7 @@ CKEDITOR_CONFIGS = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
