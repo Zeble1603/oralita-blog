975c003d8d56efc5c470ca718bb14d734bfa2962
Zeblé <66326476+Zeble1603@users.noreply.github.com>
2021-12-08 20:23:49 +0100

    Update settings.py

--- oralita/settings.py
+++ oralita/settings.py
@@ -226,9 +226,9 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+#STATICFILES_DIRS = [
+#    BASE_DIR / 'static'
+#]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
