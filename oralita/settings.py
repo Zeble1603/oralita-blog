da438b03ba544b9cd7404f49125dcf88982e13e3
Zeblé <66326476+Zeble1603@users.noreply.github.com>
2021-12-10 15:21:34 +0100

    s3 bucket config

--- oralita/settings.py
+++ oralita/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
     'blog',
     'ckeditor',
     'ckeditor_uploader',
+    'storages',
 
 ]
 
@@ -226,9 +227,12 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 
-#STATICFILES_DIRS = [
-#    BASE_DIR / 'static'
-#]
+#S3 BUCKET CONFIG
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+AWS_S3_FILE_OVERWRITE = False
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
